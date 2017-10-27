ff41715dd80ecc7330e86e938dd6ca934ff1624e
Christian Hermann <c.hermann@bitbeans.de>
2017-10-27 18:02:36 +0200

    Add more versions

--- README.md
+++ README.md
@@ -1,13 +1,33 @@
 # RedistributableChecker
 
+[![License](http://img.shields.io/badge/license-MIT-green.svg?style=flat-square)](https://github.com/bitbeans/RedistributableChecker/blob/master/LICENSE.md) [![NuGet Version](https://img.shields.io/nuget/v/RedistributableChecker.svg?style=flat-square)](https://www.nuget.org/packages/RedistributableChecker/)
 
-- Check for Visual C++ Redistributable Package 2017 (x86)
-- Check for Visual C++ Redistributable Package 2017 (x64)
+### Can checks for the following packages ###
+- Microsoft Visual C++ Redistributable Package 2017 (x86) :heavy_check_mark:
+- Microsoft Visual C++ Redistributable Package 2017 (x64) :heavy_check_mark:
+- Microsoft Visual C++ Redistributable Package 2015 (x86) :heavy_check_mark:
+- Microsoft Visual C++ Redistributable Package 2015 (x64) :heavy_check_mark:
+- Microsoft Visual C++ Redistributable Package 2013 (x86) :heavy_check_mark:
+- Microsoft Visual C++ Redistributable Package 2013 (x64) :heavy_check_mark:
+- Microsoft Visual C++ Redistributable Package 2012 (x86) :heavy_check_mark:
+- Microsoft Visual C++ Redistributable Package 2012 (x64) :heavy_check_mark:
+- Microsoft Visual C++ Redistributable Package 2010 (x86) :heavy_check_mark:
+- Microsoft Visual C++ Redistributable Package 2010 (x64) :heavy_check_mark:
+- Microsoft Visual C++ Redistributable Package 2008 (x86) :heavy_check_mark:
+- Microsoft Visual C++ Redistributable Package 2008 (x64) :heavy_check_mark:
+- Microsoft Visual C++ Redistributable Package 2005 (x86) :heavy_check_mark:
+- Microsoft Visual C++ Redistributable Package 2005 (x64) :heavy_check_mark:
 
 ```csharp 
+using RedistributableChecker;
+
 if (RedistributablePackage.IsInstalled(RedistributablePackageVersion.VC2017x64)) {
   //go on
 }
 ```
-    
-NuGet: https://www.nuget.org/packages/RedistributableChecker/0.1.0
+
+Tested on: **Windows 10 (x64)**
+   
+Windows Registry Keys taken from: https://stackoverflow.com/a/34209692/4013391 :green_heart:
+
+
--- Test/Program.cs
+++ Test/Program.cs
@@ -7,7 +7,19 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("VC2017x64 installed {0}", RedistributablePackage.IsInstalled(RedistributablePackageVersion.VC2017x64));
+			Console.WriteLine("VC2005x64 installed {0}", RedistributablePackage.IsInstalled(RedistributablePackageVersion.VC2005x64));
+			Console.WriteLine("VC2005x86 installed {0}", RedistributablePackage.IsInstalled(RedistributablePackageVersion.VC2005x86));
+			Console.WriteLine("VC2008x64 installed {0}", RedistributablePackage.IsInstalled(RedistributablePackageVersion.VC2008x64));
+			Console.WriteLine("VC2008x86 installed {0}", RedistributablePackage.IsInstalled(RedistributablePackageVersion.VC2008x86));
+			Console.WriteLine("VC2010x64 installed {0}", RedistributablePackage.IsInstalled(RedistributablePackageVersion.VC2010x64));
+			Console.WriteLine("VC2010x86 installed {0}", RedistributablePackage.IsInstalled(RedistributablePackageVersion.VC2010x86));
+			Console.WriteLine("VC2012x64 installed {0}", RedistributablePackage.IsInstalled(RedistributablePackageVersion.VC2012x64));
+			Console.WriteLine("VC2012x86 installed {0}", RedistributablePackage.IsInstalled(RedistributablePackageVersion.VC2012x86));
+			Console.WriteLine("VC2013x64 installed {0}", RedistributablePackage.IsInstalled(RedistributablePackageVersion.VC2013x64));
+			Console.WriteLine("VC2013x86 installed {0}", RedistributablePackage.IsInstalled(RedistributablePackageVersion.VC2013x86));
+			Console.WriteLine("VC2015x64 installed {0}", RedistributablePackage.IsInstalled(RedistributablePackageVersion.VC2015x64));
+			Console.WriteLine("VC2015x86 installed {0}", RedistributablePackage.IsInstalled(RedistributablePackageVersion.VC2015x86));
+			Console.WriteLine("VC2017x64 installed {0}", RedistributablePackage.IsInstalled(RedistributablePackageVersion.VC2017x64));
             Console.WriteLine("VC2017x86 installed {0}", RedistributablePackage.IsInstalled(RedistributablePackageVersion.VC2017x86));
             Console.ReadLine();
         }
